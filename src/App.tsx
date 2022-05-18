ddd6ffcc4f3ecd54e6ff35bfd9c0260834a55438
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-05-18 22:47:55 +0200

    fixed sass error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Home from "./views/Home/Home";
 import Layout from "./views/Layout/Layout";
 import Login from "./views/Login/Login";
