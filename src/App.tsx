b667704c0b685ec6cbb06aad63b8ab7d3284b0be
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-05-18 22:43:30 +0200

    added routes and starting pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./views/Home/Home";
+import Layout from "./views/Layout/Layout";
+import Login from "./views/Login/Login";
+import Register from "./views/Register/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV_mEeLZX4q6Fvh_mNespx7oJEjc8LKmY",
+  authDomain: "twitter-clone-c1e7a.firebaseapp.com",
+  projectId: "twitter-clone-c1e7a",
+  storageBucket: "twitter-clone-c1e7a.appspot.com",
+  messagingSenderId: "461504717226",
+  appId: "1:461504717226:web:ba684d48793534d675cbd3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
