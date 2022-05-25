3bb3d434bcff7e84bf7061a3450ce3b0eb5cf869
fabiantax87 <74959529+fabiantax87@users.noreply.github.com>
2022-05-25 20:04:39 +0200

    re-did the login/register system (#1)
    Co-authored-by: fabiantax87 <fabiantax87@users.noreply.github.com>

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, addDoc, collection } from "@firebase/firestore";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAV_mEeLZX4q6Fvh_mNespx7oJEjc8LKmY",
   authDomain: "twitter-clone-c1e7a.firebaseapp.com",
@@ -14,7 +11,39 @@ const firebaseConfig = {
   appId: "1:461504717226:web:ba684d48793534d675cbd3",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    const date = Date.now();
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      username: name,
+      authProvider: "local",
+      email,
+      dateCreated: date,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, logInWithEmailAndPassword, registerWithEmailAndPassword, logout };
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,16 @@
 import { useSelector } from "react-redux";
 import { selectAccount } from "../../store/account/accountSlice";
+import { logout } from "../../firebase/firebase-config.js";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/firebase-config.js";
+import { useEffect } from "react";
 
 const Home = () => {
+  const [user] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
   const Test: any = () => {
     const store = useSelector(selectAccount);
     if (store) {
@@ -11,9 +20,20 @@ const Home = () => {
     }
   };
 
+  const logUserOut = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user]);
+
   return (
     <div>
       <p>home</p>
+      <button onClick={(e) => logUserOut()}>Logout</button>
       <Test />
     </div>
   );
